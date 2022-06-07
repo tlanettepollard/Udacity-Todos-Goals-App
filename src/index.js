9f0cc8b2f566cb28f06a21101be68cf83dad512b
T. Lanette <TLPollard4516@gmail.com>
2022-06-06 21:37:15 -0400

    Update: Added all files
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import reducer from './reducers';
+import middleware from './middleware';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// createStore is deprecated; use import above
+
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
